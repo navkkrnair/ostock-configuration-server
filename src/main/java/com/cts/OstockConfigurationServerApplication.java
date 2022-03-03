1833dd4b8d6f87fe67aac9740dacc3ad4d48e7e1
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-03-03 17:31:27 +0530

    Initial commit

---
+++ src/main/java/com/cts/OstockConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstockConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstockConfigurationServerApplication.class, args);
+	}
+
+}
